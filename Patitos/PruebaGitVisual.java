4de71c8c3d49b91bdf5a78d779e92448ea546335
Piero Araujo <pieritoeeuu@gmail.com>
2026-05-01 23:55:42 -0500

    Probando conexión de Visual con Git

---
+++ Patitos/PruebaGitVisual.java
@@ -0,0 +1,9 @@
+public class SimPato {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        system.out.println("Hola Mundo");
+    }
+}
